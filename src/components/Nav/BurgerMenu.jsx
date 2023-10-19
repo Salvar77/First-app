d0aec7136b4a766487e47becfa2ed0943a9daf01
Łukasz <lukaszkus77@gmail.com>
2023-10-19 13:46:44 +0200

    update

--- src/components/Nav/BurgerMenu.jsx
+++ src/components/Nav/BurgerMenu.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Nav from "./Nav";
-import MenuIcons from "./MenuIcons";
+
 import classes from "./BurgerMenu.module.scss";
 
 const BurgerMenu = () => {
